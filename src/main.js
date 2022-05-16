d20d1cd7b187b4e6b54ca71826cb1056081bb287
yume <785879986@qq.com>
2022-05-16 17:04:19 +0800

    跟换CDN 修改 BootCDN jQuery源无法使用的问题

--- src/main.js
+++ src/main.js
@@ -3,29 +3,29 @@ const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
-    {logo: 'A', url: 'https://www.acfun.cn'},
-    {logo: 'B', url: 'https://www.bilibili.com'},
-    {logo: 'B', url: 'https://www.baidu.com'},
-    {logo: 'G', url: 'https://www.google.com'},
-    {logo: 'V', url: 'https://www.v2ex.com'},
-    {logo: 'N', url: 'http://47.101.150.38/'},
-    {logo: 'V', url: 'https://cn.vuejs.org/'},
-    {logo: 'R', url: 'https://zh-hans.reactjs.org/'},
-    {logo: 'S', url: 'https://spring.io/'},
-    {logo: 'D', url: 'https://www.djangoproject.com/'},
+	{logo: 'A', url: 'https://www.acfun.cn'},
+	{logo: 'B', url: 'https://www.bilibili.com'},
+	{logo: 'B', url: 'https://www.baidu.com'},
+	{logo: 'G', url: 'https://www.google.com'},
+	{logo: 'V', url: 'https://www.v2ex.com'},
+	{logo: 'N', url: 'https://yminami.com/'},
+	{logo: 'V', url: 'https://cn.vuejs.org/'},
+	{logo: 'R', url: 'https://zh-hans.reactjs.org/'},
+	{logo: 'S', url: 'https://spring.io/'},
+	{logo: 'D', url: 'https://www.djangoproject.com/'},
 ]
 
 const simplifyUrl = (url) => {
-    return url.replace('https://', '')
-        .replace('http://', '')
-        .replace('www.', '')
-        .replace(/\/.*/, '') //删除 / 开头的内容
+	return url.replace('https://', '')
+	          .replace('http://', '')
+	          .replace('www.', '')
+	          .replace(/\/.*/, '') //删除 / 开头的内容
 }
 
 const render = () => {
-    $siteList.find('li:not(.last)').remove()
-    hashMap.forEach((node, index) => {
-        const $li = $(`<li>
+	$siteList.find('li:not(.last)').remove()
+	hashMap.forEach((node, index) => {
+		const $li = $(`<li>
                 <div class="site">
                     <div class="logo">${node.logo}</div>
                     <div class="link">${simplifyUrl(node.url)}</div>
@@ -36,33 +36,33 @@ const render = () => {
                     </div>
                 </div>
             </li>`).insertBefore($lastLi)
-        $li.on('click', () => {
-            window.open(node.url)
-        })
-        $li.on('click', '.close', (e) => {
-            e.stopPropagation()  //阻止冒泡
-            hashMap.splice(index, 1)
-            render()
-        })
-    });
+		$li.on('click', () => {
+			window.open(node.url)
+		})
+		$li.on('click', '.close', (e) => {
+			e.stopPropagation()  //阻止冒泡
+			hashMap.splice(index, 1)
+			render()
+		})
+	});
 }
 render();
 
 $('.addButton').on('click', () => {
-    let url = window.prompt('请输入你需要添加的网址！')
-    if (url.indexOf('http') !== 0) {
-        url = 'https://www.' + url
-    }
-    hashMap.push({
-        logo: simplifyUrl(url)[0].toUpperCase(),
-        url: url
-    });
-    render();
+	let url = window.prompt('请输入你需要添加的网址！')
+	if (url.indexOf('http') !== 0) {
+		url = 'https://www.' + url
+	}
+	hashMap.push({
+		logo: simplifyUrl(url)[0].toUpperCase(),
+		url: url
+	});
+	render();
 });
 
 window.onbeforeunload = () => {
-    const string = JSON.stringify(hashMap)
-    localStorage.setItem('x', string)
+	const string = JSON.stringify(hashMap)
+	localStorage.setItem('x', string)
 }
 
 // $(document).not.on('keypress', (e) => {
